cc47c463fd483dd18d2e64a90842d9f082212aa3
Ugesh2204 <ugesh.roopchand@hotmail.com>
2021-01-15 14:28:29 +0400

    Template

--- dist/js/customsNotif.js
+++ dist/js/customsNotif.js
@@ -28,5 +28,39 @@
         timer: 50000
       });
   }
+
+
+  function   UpdateCommittee(){
+    event.preventDefault();
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Committee Updated Successfully',
+        showConfirmButton: false,
+        timer: 1500
+      });
+  }
+
+  function DeleteMember(){
+    event.preventDefault();
+    Swal.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Member removed Successfully',
+        showConfirmButton: false,
+        timer: 1500
+      });
+  }
    
+  function AddToCommittee(){
+    event.preventDefault();
+    Swal.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Member added Successfully',
+        showConfirmButton: false,
+        timer: 15000
+      });
+  }
+  
  
