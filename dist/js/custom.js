caf4d5081a68a16eabcb5f70278ecaa6d378a302
Ugesh2204 <ugesh.roopchand@hotmail.com>
2021-01-18 12:21:54 +0400

    Login

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -73,10 +73,10 @@ $('ul.nav-sidebar a').filter(function() {
     return this.href == url;
 }).addClass('active');
 
-// for treeview
-// $('ul.nav-treeview a').filter(function() {
-//     return this.href == url;
-// }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+//for treeview
+$('ul.nav-treeview a').filter(function() {
+    return this.href == url;
+}).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
 
 
 
